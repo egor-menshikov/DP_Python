ff7fe42792b004b297247f45b704f0d78676a904
Egor Menshikov <r3born.fs@gmail.com>
2023-06-04 21:05:19 +0300

    hw2 task_4 edit

--- Homeworks/HW_2/task_4/main.py
+++ Homeworks/HW_2/task_4/main.py
@@ -2,8 +2,10 @@
 # (можно прочитать в wiki что это)
 # Пример: Вводим 8
 # [-21, 13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+from typing import List
 
-def fibo(n):
+
+def fibo(n: int) -> int:
     if n == 1:
         return 1
     if n == 0:
@@ -11,7 +13,7 @@ def fibo(n):
     return fibo(n - 1) + fibo(n - 2)
 
 
-def fibo_nega_list(num):
+def fibo_nega_list(num: int) -> list[int]:
     list_fib = [fibo(i) for i in range(num + 1)]
     list_nega = list_fib[:0:-1]
     for i in range(-1, -len(list_nega) - 1, -1):
